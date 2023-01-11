4fe62cb0892207623d72d58107f0dc003c026e74
João Victor Dias <kvj1610@hotmail.com>
2023-01-11 22:39:56 +0100

    First commit

---
+++ src/main/java/com/dias/randomlykafka/RandomlyKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.dias.randomlykafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomlyKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomlyKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dias/randomlykafka/RandomlyKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dias.randomlykafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomlyKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
